a33e8b8e747787ae3cc4aad936be052bb04cc601
Onaar <paaronek00@gmail.com>
2024-02-14 18:21:54 +0100

    portals

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] Portal otherPortal;
+    [SerializeField] Material material;
+
+    public Camera myCamera;
+
+    public Transform renderSurface;
+    public Transform portalCollider;
+
+    private GameObject player;
+    private PortalTeleport portalTeleport;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        portalTeleport = portalCollider.GetComponent<PortalTeleport>();
+        portalTeleport.player = player.transform;
+        portalTeleport.receiver = otherPortal.portalCollider;
+
+        renderSurface.GetComponent<Renderer>().material = Instantiate(material);
+        if (myCamera.targetTexture != null)
+            myCamera.targetTexture.Release();
+        myCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+    }
+
+    private void Start()
+    {
+        renderSurface.GetComponent<Renderer>().material.mainTexture = otherPortal.myCamera.targetTexture;
+    }
+
+
+
+
+}
